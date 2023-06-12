80b0389c555c2d059a730fa5c60df72cad5f63c6
Andrey Kruglov <kruglov.it.rs@gmail.com>
2023-06-12 17:22:26 +0300

    1

--- src/services/store.js
+++ src/services/store.js
@@ -3,6 +3,6 @@ import userReducer from "./user";
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
     },
 });
--- src/services/user.js
+++ src/services/user.js
@@ -2,16 +2,20 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     user: null,
+    isAuthChecked: false,
 };
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload;
-        },
-    }
+  name: "user",
+  initialState,
+  reducers: {
+    setAuthChecked: (state, action) => {
+      state.isAuthChecked = action.payload;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
 });
 
 export const { setAuthChecked, setUser } = userSlice.actions;
